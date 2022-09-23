02062a3668a791f8d7bcc82f2a98db7cbf42b19c
Chukwudumebi U <79149164+chukwudumebiughonu@users.noreply.github.com>
2022-09-23 03:37:24 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,31 +1,14 @@
-Skip to content
-Gedeon-obae
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x00-hello_world/101-quote.c
-@Gedeon-obae
-Gedeon-obae that prints exactly and that piece of art is useful
- 1 contributor
-Executable File  14 lines (13 sloc)  325 Bytes
 #include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - prints exactly "and that piece of art is useful"
- * - Gideon Obae, 2021-08-26",
+ * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: 1 if success.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
